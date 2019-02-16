e432c2244e4d0b6825e0584ef7a5eebac43eb699
danataioutlook <47568383+danataioutlook@users.noreply.github.com>
2019-02-16 11:07:37 +0600

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created txt for check
+            string s = @"TextFile4.txt";
+            //created file
+            var c = File.Create(@"C:\Users\user\source\repos\ConsoleApp4\ConsoleApp4\Create\" + s);
+            c.Close();
+            //copied first file and deleted first file
+            File.Copy(@"C:\Users\user\source\repos\ConsoleApp4\ConsoleApp4\Create\" + s, @"C:\Users\user\source\repos\ConsoleApp4\ConsoleApp4\Create1\" + s);
+            File.Delete(@"C:\Users\user\source\repos\ConsoleApp4\ConsoleApp4\Create\" + s);
+
+        }
+    }
+}
